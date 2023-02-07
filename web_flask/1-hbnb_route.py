e954bc05c3eb09654868f40cfd3e40b2c0dc19f9
Samuel Akwensivie <sakwnesivie@gmail.com>
2023-02-07 04:34:56 +0000

    hbnb route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!.usr/bin/python3
+'''Script that runs an app with Flask Framework'''
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strick_slashes=False)
+def hello_hbnb():
+    '''Function called with / route'''
+    return 'Hello HBNB'
+
+
+@app.route('/hbnb', strick_slashes=False)
+def hbnb():
+    '''Function called with /hbnb route'''
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
